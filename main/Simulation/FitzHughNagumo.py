4cfcde3f631416fd7c3360e4c20ac2c27cbed0de
mjrandal <mjrandal@iu.edu>
2023-05-02 11:57:26 -0400

    version 4

--- main/Simulation/FitzHughNagumo.py
+++ main/Simulation/FitzHughNagumo.py
@@ -1,7 +1,10 @@
+
 from cc3d import CompuCellSetup
+        
 
-from FitzHughNagumoSteppables import FitzHughNagumoSteppable
+from FitzHughNagumoSteppables import Projectv1Steppable
 
-CompuCellSetup.register_steppable(steppable=FitzHughNagumoSteppable(frequency=1))
+CompuCellSetup.register_steppable(steppable=Projectv1Steppable(frequency=1))
+
 
 CompuCellSetup.run()
--- main/Simulation/FitzHughNagumoSteppables.py
+++ main/Simulation/FitzHughNagumoSteppables.py
@@ -1,11 +1,18 @@
+from cc3d.cpp.PlayerPython import * 
+from cc3d import CompuCellSetup
+
+
 from cc3d.core.PySteppables import *
 import numpy as np
 
-class FitzHughNagumoSteppable(SteppableBasePy):
+class Projectv1Steppable(SteppableBasePy):
 
     def __init__(self,frequency=1):
 
         SteppableBasePy.__init__(self,frequency)
+
+        
+        
 
     def start(self):
         """
@@ -15,63 +22,84 @@ class FitzHughNagumoSteppable(SteppableBasePy):
         self.Fieldu_threshold=4
         self.Fieldu_threshold2=2
         self.Fieldv_threshold=5
-        self.creationrate=0.3
+        self.creationrate=0.9
+        #old =0.3
+        self.XDThresh=0.8
+        self.MDThresh=0.8
+        self.MCThresh=2.0
+        self.MCThresh2=0
+        self.XCThresh=0
+        self.XCThresh2=0
+        
     def step(self,mcs):
         """
         type here the code that will run every frequency MCS
         :param mcs: current Monte Carlo step
         """
-        totvolume=0
+        totvolume=0  #volume check to make sure entire area not filled (may not need)
         for cell in self.cell_list:
-            totvolume+=cell.volume
-            ucon=self.field.u[cell.xCOM,cell.yCOM,0]
-            vcon=self.field.v[cell.xCOM,cell.yCOM,0]
-            if cell.type==self.MELANOPHORES and ucon<self.Fieldu_threshold2:
-                #try dying celltype
-                #totvolume-=cell.volume
-                #self.delete_cell(cell)
+            
+            totvolume+=cell.volume  #volume incrementer
+
+            
+            #concentrations of fields in each cell
+            LMelcon=self.field.Melanophore[cell.xCOM,cell.yCOM,0]
+            LXancon=self.field.Xanthophore[cell.xCOM,cell.yCOM,0]
+            SMelcon=self.field.Melanophores2[cell.xCOM,cell.yCOM,0]
+            SXancon=self.field.Xanthophores2[cell.xCOM,cell.yCOM,0]
+            
+
+            #cell type changes if no longer in correct location
+            if cell.type==self.MELANOPHORES and (LXancon<SXancon):
                 cell.type=self.MELANOPHORES2
-                
-                
-            if cell.type==self.XANTHOPHORES and ucon>self.Fieldu_threshold:
-                #try dying celltype
-                #totvolume-=cell.volume
-                #self.delete_cell(cell)
+            if cell.type==self.XANTHOPHORES and (LMelcon<SMelcon):
                 cell.type=self.XANTHOPHORES2
-            if mcs%100==0:
-                if cell.type==self.MELANOPHORES2 and ucon<self.Fieldu_threshold2:
-                    totvolume-=cell.volume
-                    self.delete_cell(cell)  
-                
-                if cell.type==self.XANTHOPHORES2 and ucon>self.Fieldu_threshold:
+            
+            #kills or reverts cells every 300 mcs (should count for every cell, but likely too slow)
+            if mcs%300==0:
+                if cell.type==self.MELANOPHORES2 and LXancon<SXancon:
                     totvolume-=cell.volume
                     self.delete_cell(cell)
-                if cell.type==self.MELANOPHORES2 and ucon>self.Fieldu_threshold:
+                elif cell.type==self.MELANOPHORES2:
                     cell.type=self.MELANOPHORES
-                if cell.type==self.XANTHOPHORES2 and ucon<self.Fieldu_threshold2:
-                    cell.type=self.XANTHOPHORES
+                if cell.type==self.XANTHOPHORES2 and LMelcon<SMelcon:
+                    totvolume-=cell.volume
+                    self.delete_cell(cell)
+                elif cell.type==self.XANTHOPHORES2:
+                  cell.type=self.XANTHOPHORES  
+                    
+                
+            
+
                 
         print(self.pixel_tracker_plugin.getMediumPixelSet())
-        if mcs>1000 and np.random.uniform()<1 - np.exp(-self.creationrate*1) and totvolume < 0.95*(self.dim.x*self.dim.y*1): #and len(self.pixel_tracker_plugin.getMediumPixelSet())>0.1*(self.dim.x*self.dim.y*1):
+        
+        #cell seeding
+        if np.random.uniform()<1 - np.exp(-self.creationrate*1) and totvolume < 0.95*(self.dim.x*self.dim.y*1): #and len(self.pixel_tracker_plugin.getMediumPixelSet())>0.1*(self.dim.x*self.dim.y*1):
             xseed=np.random.randint(self.dim.x)
             yseed=np.random.randint(self.dim.y)
             occupied= bool(self.cell_field[xseed,yseed,0])
             
+            #occupied pixel check
             while occupied:
                 xseed=np.random.randint(self.dim.x)
                 yseed=np.random.randint(self.dim.y)
                 occupied= bool(self.cell_field[xseed,yseed,0])
             
             
-            if self.field.u[xseed,yseed,0] > self.Fieldu_threshold:
 
+            
+            
+            #seeing according to paper parameters
+            if self.field.Melanophores2[xseed,yseed,0]>self.field.Melanophore[xseed,yseed,0] or (self.field.Xanthophores2[xseed,yseed,0]<self.field.Xanthophore[xseed,yseed,0] and self.field.Xanthophore[xseed,yseed,0]>0.1):
                 cell = self.new_cell(self.MELANOPHORES)
-                # size of cell will be 1x1x1
                 self.cell_field[xseed, yseed, 0] = cell
-            elif self.field.u[xseed,yseed,0] < self.Fieldu_threshold:
+
+            elif self.field.Melanophores2[xseed,yseed,0]<self.field.Melanophore[xseed,yseed,0] and self.field.Melanophore[xseed,yseed,0]>0.1 or (self.field.Xanthophores2[xseed,yseed,0]>self.field.Xanthophore[xseed,yseed,0]):
                 cell = self.new_cell(self.XANTHOPHORES)
-                # size of cell will be 1x1x1
                 self.cell_field[xseed, yseed, 0] = cell
+
+
                 
                 
         
